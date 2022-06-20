6558c2beb3c57d9bb287d997762c27b123152111
Raghad Karasneh <raghad.karasneh@gmail.com>
2022-06-20 17:10:37 +0300

    React Tasks

---
+++ Monday20/6/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import SecondTask from './SecondTask';
+import FirstTask from './FirstTask';
+
+function App() {
+  return (
+    <>
+    <SecondTask />
+    <FirstTask />
+    </>
+  );
+}
+
+export default App;

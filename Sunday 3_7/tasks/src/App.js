a9427c5daea0e7cab7ca23a29075b8b027da7673
Raghad Karasneh <raghad.karasneh@gmail.com>
2022-07-04 07:16:11 +0300

    Netflix

--- Sunday 3_7/tasks/src/App.js
+++ Sunday 3_7/tasks/src/App.js
@@ -1,11 +1,18 @@
 import logo from './logo.svg';
+import Navbar from './Components/Navbar/Navbar';
+import Banner from "./Components/Banner/Banner"
 import Home from './Home'
+import SingleMovie from './Components/SingleMovie/SingleMovie';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
+       <Banner />
      <Home/>
+    <h1>Single movie</h1>
+    <SingleMovie />
     </div>
   );
 }
--- Sunday 3_7/tasks/src/Movie.js
+++ Sunday 3_7/tasks/src/Movie.js
@@ -2,8 +2,10 @@ import React from 'react';
 const Movie = ({movie}) => {
     return (
       <div>
-        <h5>{movie.title}</h5>
+        <h3>{movie.original_title}</h3>
         <img src={"https://image.tmdb.org/t/p/w500"+ movie.backdrop_path} alt="" />
+        <p>{movie.overview}</p>
+        <p>Rate:{movie.vote_average}</p>
       </div>
     );
   };
